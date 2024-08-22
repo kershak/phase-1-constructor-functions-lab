f4e58dfcbd2817edb3d4d426cb9d60ce504ca1e5
Nills <yakusaz17@gmail.com>
2024-08-22 08:11:37 -0400

    completed

---
+++ index.js
@@ -0,0 +1,16 @@
+function scooter (year, color, model){
+    this.year =year;
+    this.color = color;
+    this.model = model;
+}
+
+function driver (name, age,experience){
+    this.name = name;
+    this.age = age;
+    this.experience = experience;
+}
+
+function pickupLocation (address, city){
+    this.address = address;
+    this.city = city;
+}
